46b8a4ffc604dfeea481da23ec7f413af9dfcad5
William Wicks <wjwicks@gmail.com>
2013-09-19 17:13:48 +0000

    reworked JOIN/PART/QUIT fix #1

--- channels.js
+++ channels.js
@@ -1,6 +1,8 @@
 
 var irc = require("irc-connect");
 var EventEmitter = require("events").EventEmitter;
+var debug = require("debug")('irc-channel');
+var push = Array.prototype.push;
 
 function msg(msg){
 	if(this.__active)
@@ -38,107 +40,130 @@ module.exports = {
 			}
 		};
 		client
-			.on('JOIN', handler)
+			.on('JOIN', onjoin)
+			.on('QUIT', onquit)
 			.on('PART', handler)
 			.on('KICK', handler)
 			.on('PRIVMSG', handler)
 			.on('TOPIC', handler)
-			.on('QUIT', function (event) {
-				var who = event.nick;
-				var isYou = who===this.nick();
-
-				Object.keys(this.channels).forEach(function (channel) {
-					channel = client.channels[channel];
-					if(isYou) channel.__active = false;
-					var idx = channel.names.indexOf(who);
-					if(~idx) {
-						channel.names.splice(idx, 1);
-						channel.emit('QUIT', event);
-						channel.emit('data', event);
-					}
-				});
-			})
 		;
 	}
 }
-
 function handler(event){
-	var isYou = event.nick===this.nick();
 	var name = event.params[0].toLowerCase();
-
 	var channel = this.channels[name];
-
-	if(isYou && event.command==='JOIN'){
-		if(!channel){
-			channel = this.channels[name] = new EventEmitter();
-			channel.client = this;
-			channel.send = this.send.bind(this);
-			channel.name = name;
-			channel.msg = msg;
-			channel.part = part;
-			channel.topic = {};
-			irc.names.__irc(channel);
-		}
-		channel.__active = true;
-		var names = channel.names = [];
-
-		this.on('RPL_TOPIC', intro);
-		this.on('RPL_TOPIC_WHO_TIME', intro);
-		this.on('RPL_ENDOFNAMES', intro);
-
-		this.on('RPL_NAMREPLY', namereply);
-		function namereply(event){
-			if(event.params[2].toLowerCase()!==name) return;
-			names.push.apply(names, event.params[3].split(' '));
-		}
-
-		function intro(event){
-			if(event.params[1].toLowerCase()!==name) return;
-
-			this.removeListener(event.command, intro);
-			switch(event.command){
-				case 'RPL_TOPIC':
-					channel.topic.message = event.params[2];
-					break;
-				case 'RPL_TOPIC_WHO_TIME':
-					channel.topic.by = event.params[2];
-					channel.topic.at = event.params[3];
-					break;
-				case 'RPL_ENDOFNAMES':
-					//make sure everything is removed
-					this.removeListener('RPL_TOPIC', intro);
-					this.removeListener('RPL_TOPIC_WHO_TIME', intro);
-					this.removeListener('RPL_ENDOFNAMES', intro);
-					this.removeListener('RPL_NAMREPLY', namereply);
-					channel.emit('joined');
-					break;
-			}
-		}
-	}
 	if(!channel) return;
 
-	if(isYou && event.command==='PART'){
-		channel.__active = false;
-		delete this.channels[event.params[0]];
+	if(event.command==='PART'){
+		var isYou = event.nick===this.nick();
+		removeName(this, channel, isYou, event);
+		return;
 	}
-	channel.emit('PART', event);
+	emit(channel, event);
+}
+
+function onjoin(event){
+	var isYou = event.nick===this.nick();
+	var cname = event.params[0].toLowerCase();
+
+	if(isYou){
+		var channel = initChannel(this, cname);
+	}
+	else {
+		var channel = this.channels[cname];
+		if(!channel) return;
+
+		var names = channel.names;
+		if(event.nick in names)
+			debug[cname]('attempt to add duplicate name: ' + event.nick);
+		else
+			names.push(event.nick);
+	}
+	emit(channel, event);
+}
+function initChannel(client, cname){
+	var dbg = debug[cname] = require("debug")('irc-channel:'+cname);
+	dbg('channel create');
+	var channel = client.channels[cname] = new EventEmitter();
+	channel.client = client;
+	channel.send = client.send.bind(client);
+	channel.name = cname;
+	channel.msg = msg;
+	channel.part = part;
+	channel.topic = {};
+	channel.__active = true;
+	channel.names = [];
+
+	client.on('RPL_NAMREPLY', namereply);
+	function namereply(event){
+		if(event.params[2].toLowerCase()!==cname)
+			return;
+
+		var names = event.params[3].split(' ');
+		if(dbg.enabled) dbg(JSON.stringify(event));
+		push.apply(channel.names, names);
+	}
+
+	client.on('RPL_TOPIC', intro);
+	client.on('RPL_TOPIC_WHO_TIME', intro);
+	client.on('RPL_ENDOFNAMES', intro);
+	function intro(event){
+		if(event.params[1].toLowerCase()!==cname) return;
+
+		if(dbg.enabled) dbg(JSON.stringify(event));
+
+		client.removeListener(event.command, intro);
+
+		switch(event.command){
+			case 'RPL_TOPIC':
+				channel.topic.message = event.params[2];
+				break;
+			case 'RPL_TOPIC_WHO_TIME':
+				channel.topic.by = event.params[2];
+				channel.topic.at = event.params[3];
+				break;
+			case 'RPL_ENDOFNAMES':
+				dbg('There are ' +channel.names.length+ ' channel members');
+				//make sure everything is removed
+				client.removeListener('RPL_TOPIC', intro);
+				client.removeListener('RPL_TOPIC_WHO_TIME', intro);
+				client.removeListener('RPL_ENDOFNAMES', intro);
+				client.removeListener('RPL_NAMREPLY', namereply);
+				channel.emit('joined');
+				break;
+		}
+	}
+	return channel;
+}
+
+function onquit(event){
+	var who = event.nick;
+	var isYou = who===this.nick();
+	var client = this;
+
+	Object.keys(this.channels).forEach(function (cname) {
+		var channel = client.channels[cname];
+		removeName(client, channel, isYou, event);
+	});
+}
+function removeName(client, channel, isYou, event) {
+
+	var idx = channel.names.indexOf(event.nick);
+	if(~idx) {
+		channel.names.splice(idx, 1);
+		emit(channel, event);
+	}
+
+	if(isYou) {
+		channel.__active = false;
+		delete client.channels[event.params[0]];
+		dbg('channel removed');
+	}
+}
+
+function emit(channel, event) {
+	var dbg = debug[channel.name];
+	if(dbg.enabled) dbg(JSON.stringify(event));
+	channel.emit(event.command, event);
 	channel.emit('data', event);
-}
-
-function end(event) {
-	if(!event)
-		return this.emit('names', 'timeout', null, channel);
-
-	var names = this.__names;
-	delete this.__names;
-	this.emit('names', null, names);
-}
-
-function reply(event){
-	if(!this.__names) this.__names = {};
-	var names = this.__names;
-	var chan = event.params[2].toLowerCase();
-
-	chan = names[chan] || (names[chan] = []);
-	chan.push.apply(chan, event.params[3].split(' '));
 }
