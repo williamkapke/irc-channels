71e246cd035391f4b93ad4fbe5228a695b3d2d61
William Wicks <wjwicks@gmail.com>
2013-09-19 00:11:19 +0000

    v3.5.8

---
+++ README.md
@@ -0,0 +1,75 @@
+irc-channels
+============
+
+Channel features for [irc-connect](https://github.com/williamwicks/irc-connect).
+
+```javascript
+var channels = require('irc-channels');
+var irc = require("irc-connect");
+
+irc.connect('irc.freenode.net')
+	.use(irc.pong, channels)
+	.on('welcome', function (msg) {
+		console.log(msg);
+
+		this.join('#storehours', function(channel){
+
+			channel.on('data', function (event) {
+				console.log(JSON.stringify(event));
+			});
+
+			console.log(channel.topic);
+			channel.msg('I got https://npmjs.org/package/irc-channels working!');
+		})
+	})
+;
+```
+
+
+
+### The channel object
+It is an `EventEmitter` with some extras.
+
+#### channel.send(text[,text]...)
+A proxy to `irc-connect`'s `send()` function;
+It sends the text followed by a `\n`.
+
+#### channel.name
+Gets the name of the channel.
+
+#### channel.msg
+Sends a `PRIVMSG` to the channel.
+
+#### channel.part
+Sends a `PRIVMSG` to the channel.
+
+#### channel.topic
+Gets current topic of the channel. Returns an `object`.
+
+```javascript
+console.log(JSON.stringify(channel.topic, null, 2));
+//{
+//  "message": "Hello irc-connect ppl",
+//  "by": "wwicks!~wwicks@0.0.0.0",
+//  "at": "1379547726"
+//}
+```
+
+#### channel.names
+Gets and `Array` of the current channel members. The list is updated when
+`QUIT` | `JOIN` | `PART` happen.
+
+## Events
+The channel will emit the following channel specific events:
+
+#### Event:'JOIN'
+#### Event:'PART'
+#### Event:'KICK'
+#### Event:'PRIVMSG'
+#### Event:'TOPIC'
+#### Event:'QUIT'
+#### Event:'names'
+Emitted after the names list has been parsed.
+
+### license
+MIT
---
+++ channels.js
@@ -0,0 +1,144 @@
+
+var irc = require("irc-connect");
+var EventEmitter = require("events").EventEmitter;
+
+function msg(msg){
+	if(this.__active)
+		this.send('PRIVMSG ', this.name, ' :', msg);
+	return this;
+}
+function part(msg){
+	if(!this.__active) return;
+
+	if(msg) this.send('PART ', this.name, ' :', msg);
+	else this.send('PART ', this.name);
+}
+
+module.exports = {
+	__irc: function(client){
+		client.channels = {};
+		client.join = function(name, cb){
+			name = name && name.toLowerCase();
+			if(!name || name in this.channels) return;
+
+			this.send('JOIN ', name);
+
+			if(!cb) return;
+
+			this.on('JOIN', join);
+			function join(){
+				var channel = this.channels[name];
+				if(!channel) return;
+				this.removeListener('JOIN', join);
+
+				//wait for names and topic to finish
+				channel.once('joined', function (){
+					cb.call(channel, channel);
+				});
+			}
+		};
+		client
+			.on('JOIN', handler)
+			.on('PART', handler)
+			.on('KICK', handler)
+			.on('PRIVMSG', handler)
+			.on('TOPIC', handler)
+			.on('QUIT', function (event) {
+				var who = event.nick;
+				var isYou = who===this.nick();
+
+				Object.keys(this.channels).forEach(function (channel) {
+					channel = client.channels[channel];
+					if(isYou) channel.__active = false;
+					var idx = channel.names.indexOf(who);
+					if(~idx) {
+						channel.names.splice(idx, 1);
+						channel.emit('QUIT', event);
+						channel.emit('data', event);
+					}
+				});
+			})
+		;
+	}
+}
+
+function handler(event){
+	var isYou = event.nick===this.nick();
+	var name = event.params[0].toLowerCase();
+
+	var channel = this.channels[name];
+
+	if(isYou && event.command==='JOIN'){
+		if(!channel){
+			channel = this.channels[name] = new EventEmitter();
+			channel.client = this;
+			channel.send = this.send.bind(this);
+			channel.name = name;
+			channel.msg = msg;
+			channel.part = part;
+			channel.topic = {};
+			irc.names.__irc(channel);
+		}
+		channel.__active = true;
+		var names = channel.names = [];
+
+		this.on('RPL_TOPIC', foo);
+		this.on('RPL_TOPIC_WHO_TIME', foo);
+		this.on('RPL_ENDOFNAMES', foo);
+
+		this.on('RPL_NAMREPLY', namereply);
+		function namereply(event){
+			if(event.params[2].toLowerCase()!==name) return;
+			names.push.apply(names, event.params[3].split(' '));
+		}
+
+		function foo(event){
+			if(event.params[1].toLowerCase()!==name) return;
+
+			this.removeListener(event.command, foo);
+			switch(event.command){
+				case 'RPL_TOPIC':
+					channel.topic.message = event.params[2];
+					break;
+				case 'RPL_TOPIC_WHO_TIME':
+					channel.topic.by = event.params[2];
+					channel.topic.at = event.params[3];
+					break;
+				case 'RPL_ENDOFNAMES':
+					//make sure everything is removed
+					this.removeListener('RPL_TOPIC', foo);
+					this.removeListener('RPL_TOPIC_WHO_TIME', foo);
+					this.removeListener('RPL_ENDOFNAMES', foo);
+					this.removeListener('RPL_NAMREPLY', namereply);
+					channel.emit('joined');
+					break;
+			}
+		}
+	}
+	if(!channel) return;
+
+	if(isYou && event.command==='PART'){
+		channel.__active = false;
+		delete this.channels[event.params[0]];
+	}
+	channel.emit('PART', event);
+	channel.emit('data', event);
+}
+
+function end(event) {
+	if(!event)
+		return this.emit('names', 'timeout', null, channel);
+
+	var names = this.__names;
+	delete this.__names;
+	this.emit('names', null, names);
+}
+
+function reply(event){
+	if(!this.__names) this.__names = {};
+	var names = this.__names;
+	var chan = event.params[2].toLowerCase();
+
+	chan = names[chan] || (names[chan] = []);
+	chan.push.apply(chan, event.params[3].split(' '));
+}
